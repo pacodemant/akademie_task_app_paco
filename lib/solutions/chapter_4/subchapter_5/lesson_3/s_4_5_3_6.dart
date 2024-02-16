62d52c08e977436d49c4ae11c51297ca541bb002
pacodemant <148328057+pacodemant@users.noreply.github.com>
2024-02-16 21:21:38 +0100

    solution 4536 (forgot to push)

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -18,41 +18,63 @@ class Product {
 }
 
 /// The MyProductList class is a stateless widget in Dart.
-class MyProductList extends StatelessWidget {
+class MyProductList extends StatefulWidget {
   const MyProductList({super.key});
 
-
-
   @override
-  Widget build(BuildContext context) {
-
-/// The line `List<Product> productList=[];` is declaring a variable named `productList` of type
-/// `List<Product>`. It is initializing the list with an empty list literal `[]`. This means that
-/// `productList` is an empty list that can hold objects of type `Product`.
-  List<Product> productList=[]; 
-
-
-    return const ListTile(
-      tileColor: Colors.deepOrange,
-      titleAlignment: ListTileTitleAlignment.center,
-      leading: Icon(Icons.shopping_cart),
-      title: Text(
-        "Produkt Nr. ",
-        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-      ),
-      trailing: Text("- ${productList.}"),
-    );
-  }
+  State<MyProductList> createState() => _MyProductListState();
 }
 
-/// The main function is the entry point of a Dart program.
-void main(){
+class _MyProductListState extends State<MyProductList> {
+  @override
+  Widget build(BuildContext context) {
+    List<Product> productList = []; // Liste von Produkten
 
-  
-  for (int i = 0; i < 4; i++) {
-    productList.add(Product("$i", i*1.1));
-    //** */ Product("$i", i*1.1);
+    /// The `for` loop is iterating over a range of numbers from 0 to 4 (inclusive). In each iteration, it
+    /// creates a new product name and price based on the current index, and adds a new `Product` object to
+    /// the `productList` list.
+    for (int i = 0; i < 5; i++) {
+      String productName = 'B-Product ${i + 1}';
+      double productPrice = (i + 1) * 1.11; // Beispiel Preislogik
+      productList.add(Product(productName, productPrice));
+    }
+
+    return ListView.builder(
+      itemCount: 5,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        //Product product = productList[index];
+        return ListTile(
+          tileColor: Colors.deepOrange,
+          titleAlignment: ListTileTitleAlignment.center,
+          leading: const Icon(Icons.shopping_cart),
+          /*leading:
+            GestureDetector(onTap: () {
+            setState(() {
+              iconColor = Colors.red;
+            });
+          },) */
+          iconColor: Colors.cyan,
+          title: Text(
+            productList[index].name,
+            style: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.yellowAccent),
+          ),
+          trailing: Text(productList[index].price.toStringAsFixed(2)),
+          textColor: Colors.white,
+        );
+      },
+    );
+
+    /* return  ListTile(
+      tileColor: Colors.deepOrange,
+      titleAlignment: ListTileTitleAlignment.center,
+      leading: Icon(Icons.shopping_cart), iconColor: Colors.black54,
+      title: Text(
+        productList[index].name,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.blue),
+      ),
+      trailing: Text(productList[index].price.toStringAsFixed(2)), textColor: Colors.white,
+    ); */
   }
-
-
-} 
+}
