10884c00c5d8eb8de38891bf77347fa3332964a7
pacodemant <148328057+pacodemant@users.noreply.github.com>
2024-02-27 22:53:21 +0100

    5.1.3.2 gelöst

--- lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_3/s_5_1_3_2.dart
@@ -20,19 +20,30 @@ class _FoldingUnfoldingTextState extends State<FoldingUnfoldingText> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 30,
+      height: heightTextbox,
       width: 200,
       child: InkWell(
         onTap: () {
-          // ignore: unused_local_variable
-          int a = 1;
+          // ignore: unused_local_variable, avoid_print
+          print('Klick funzt');
+          _toggleButton();
         },
         child: const Text(
-          'Erstelle ein `Text`-Widget in einer `SizedBox`, die 200px breit und 30px hoch ist. Der Text sollte länger als die Box sein und deshalb nach unten hin den überlaufenden Text ausblenden. Integriere die Funktionalität, dass der Text sich bei einem Klick darauf ausklappt.',
+          'Click 2 fold out:\nErstelle ein `Text`-Widget in einer `SizedBox`, die 200px breit und 30px hoch ist. Der Text sollte länger als die Box sein und deshalb nach unten hin den überlaufenden Text ausblenden. Integriere die Funktionalität, dass der Text sich bei einem Klick darauf ausklappt.',
           overflow: TextOverflow.fade,
         ),
       ),
     );
   }
 
+  double heightTextbox = 30;
+
+  // ignore: unused_element
+  void _toggleButton() {
+    setState(
+      () {
+        heightTextbox = heightTextbox == 30 ? 200 : 30;
+      },
+    );
+  }
 }
